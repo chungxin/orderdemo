115fee4dd83fae5244c2916f5054618be5635fe4
chungxin <1103137137@gm.kuad.edu.tw>
2017-03-24 13:53:21 +0800

    tempdata

--- eSale/Controllers/OrderController.cs
+++ eSale/Controllers/OrderController.cs
@@ -12,10 +12,11 @@ namespace eSale.Controllers
         // GET: /Order/
         public ActionResult Index()
         {
-            Models.OrderServer orderService = new Models.OrderServer();
-            var order = orderService.GetOrderById("111");
-            ViewBag.CustId = order.CustId;
-            ViewBag.CustName = order.CustName;
+           // Models.OrderServer orderService = new Models.OrderServer();
+           // var order = orderService.GetOrderById("111");
+           // ViewBag.CustId = order.CustId;
+
+       
             return View();
         }
 
@@ -26,11 +27,25 @@ namespace eSale.Controllers
         [HttpPost()]
         public ActionResult InsertOrder(Models.Order order)
         {
-            Models.OrderServer orderService = new Models.OrderServer();
-            orderService.InsertOrder(order);
-            return View("Index");
+            ViewBag.Desc1 = "我是viewbag";
+            ViewData["Desc2"] = "我是viewdata";
+            TempData["Desc3"] = "我是Tempdata";
+
+            return RedirectToAction("Index");
         }
-       
+    [HttpGet()]
+       public JsonResult TestJson()
+        {
+
+            var result = new Models.Order()
+            {
+                CustId = "GSS",
+                CustName = "hi"
+            };
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
 
 	}
 }
